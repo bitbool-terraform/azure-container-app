a1e9eeb6c0eeb3861946be0f3aa543e5a3d9dc2c
Antonis Antonopoulos <pseudorandomized@gmail.com>
2025-07-01 14:06:43 +0300

    fixed identities gathering

--- data.identities.tf
+++ data.identities.tf
@@ -6,7 +6,9 @@ for_each = { for k,v in local.secrets_selected: k=>v if lookup(v,"identity",null
 }
 
 data "azurerm_user_assigned_identity" "app_id" {
-  for_each            = var.identities != null ? toset(var.identities) : []
+  for_each            = var.identities != null ? toset(local.identities_full_list) : []
+  # for_each            = var.identities != null ? toset(var.identities) : []
+
   name                = each.value
   resource_group_name = var.resource_group
 }
